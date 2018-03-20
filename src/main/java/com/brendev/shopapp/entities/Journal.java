d1fea00f87e917cb85d5ff7c7a5cd8d8c1b68f47
PAKOU Komla Nomessi <pakounomessi@gmail.com>
2018-03-20 08:49:25 +0000

    test

--- src/main/java/com/brendev/shopapp/entities/Journal.java
+++ src/main/java/com/brendev/shopapp/entities/Journal.java
@@ -26,8 +26,8 @@ import javax.persistence.TemporalType;
 @Table(name = "journals")
 public class Journal implements Serializable{
     @Id
-    //@SequenceGenerator(name = "journalSeq", sequenceName = "JOURNAL_SEQ", allocationSize = 1, initialValue = 1)
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @SequenceGenerator(name = "journalSeq", sequenceName = "JOURNAL_SEQ", allocationSize = 1, initialValue = 1)
+    @GeneratedValue(strategy = GenerationType.AUTO,generator = "journalSeq")
     @Column(name = "ID", nullable = false)
     private Long idJourn;
     
